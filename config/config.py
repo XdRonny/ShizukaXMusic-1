fadcce197e07818328479a578b9eabfd0e5f9772
XdRonny <127653254+XdRonny@users.noreply.github.com>
2023-05-17 05:02:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,7 +27,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/itz-star-boi/ShizukaXMusic",
+    "https://github.com/XdRonny/ShizukaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GITHUB_REPO = getenv("GITHUB_REPO", "https://te.legra.ph/file/8bc18c07df1361a31b88a.jpg")
